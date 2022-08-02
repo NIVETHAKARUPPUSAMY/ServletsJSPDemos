75ed894b154f30202d546ffa5c9e4d2316c72569
NIVETHA KARUPPUSAMY <nivetha5642@gmail.com>
2022-08-02 12:29:43 +0530

    changes

--- JDBC/src/JDBCDemo/ReadAndWriteSql.java
+++ JDBC/src/JDBCDemo/ReadAndWriteSql.java
@@ -1,5 +1,73 @@
 package JDBCDemo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class ReadAndWriteSql {
+public class ReadAndWriteSql  {
+
+	public static void main(String[] args) {
+		
+		InsertintoDB();
+		
+		updateDB();
+		
+		readDB();
+		
+		deleteDB();
+
+	}
+
+	private static void deleteDB() {
+		try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","Nivetha@5642");
+				Statement st = con.createStatement();) {
+			int rowsdeleted = st.executeUpdate("delete from account where accno = 1");
+			System.out.println("Number of rows deleted"+rowsdeleted);
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static void readDB() {
+		
+		try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","Nivetha@5642");
+				Statement st = con.createStatement();) {
+			ResultSet rs = st.executeQuery("Select * from account");
+			while(rs.next()) {
+				System.out.println(rs.getInt(1)+","+rs.getString(2)+","+rs.getString(3)+","+rs.getInt(4));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static void updateDB() {
+		
+		try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","Nivetha@5642");
+				Statement st = con.createStatement();) {
+			int rowsupdated = st.executeUpdate("update account set balance=150000 where accno = 2");
+			System.out.println("Number of rows updated"+rowsupdated);
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void InsertintoDB() {
+		try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","Nivetha@5642");
+				Statement st = con.createStatement();) {
+			int rowsinserted = st.executeUpdate("insert into account values(3,'nkk','abc',10000)");
+			System.out.println("Number of rows inserted"+rowsinserted);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 }
+
+
